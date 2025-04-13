be7e83015aeec566daaca63c82b43babfdd11c30
jnccd <kobidogao@outlook.com>
2025-04-13 14:47:32 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -19,39 +19,35 @@
 
     hosts = [
       { hostname = "lt-coffeelake"; system = "x86_64-linux"; stateVersion = "24.11"; }
+      { hostname = "nixos"; system = "x86_64-linux"; stateVersion = "24.11"; }
     ];
-
-    mkSystem = { hostname, system, stateVersion, homeStateVersion, username }: nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit inputs stateVersion hostname username;
-      };
-
-      modules = [
-        ./hosts/${hostname}/configuration.nix
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {
-            inherit inputs hostname homeStateVersion username;
-          };
-          home-manager.users.${username} = {
-            imports = [
-              ./hosts/${hostname}/home.nix
-            ];
-          };
-        }
-      ];
-    };
   in {
     nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
       configs // {
-        "${host.hostname}" = mkSystem {
-          inherit (host) hostname system stateVersion;
-          inherit homeStateVersion;
-          inherit username;
+        "${host.hostname}" = nixpkgs.lib.nixosSystem {
+          inherit (host) system;
+          specialArgs = {
+            inherit inputs username;
+            inherit (host) hostname stateVersion;
+          };
+
+          modules = [
+            ./hosts/${host.hostname}/configuration.nix
+          ];
+        };
+      }) {} hosts;
+
+    homeConfigurations = nixpkgs.lib.foldl' (configs: host:
+      configs // {
+        "dobiko@${host.hostname}" = {
+          pkgs = nixpkgs.legacyPackages.${system};
+          extraSpecialArgs = {
+            inherit inputs homeStateVersion user;
+          };
+
+          modules = [
+            ./hosts/${host.hostname}/home.nix
+          ];
         };
       }) {} hosts;
   };
