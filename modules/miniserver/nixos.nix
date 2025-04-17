ba617b3481b203bf868da78487a4c2a6c4354516
jnccd <kobidogao@outlook.com>
2025-04-17 19:47:36 +0200

    Test service

--- modules/miniserver/nixos.nix
+++ modules/miniserver/nixos.nix
@@ -4,23 +4,23 @@
     enable = true;
     description = sessionName;
 
-    requires = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
+    requires = [ "network-online.target" ];
 
     serviceConfig = {
       User = user;
 
       ExecStart = pkgs.writeScript "${sessionName}-start-script" ''
-            #!${pkgs.runtimeShell}
-            PATH=$PATH:/run/current-system/sw/bin
-            screen -S ${sessionName} -dm bash -c "${script}";
-            while :; do sleep 2073600; done
-          '';
-      ExecStop = pkgs.writeScript "${sessionName}-start-script" ''
-            #!${pkgs.runtimeShell}
-            PATH=$PATH:/run/current-system/sw/bin
-            screen -XS ${sessionName} quit
-          '';
+          #!${pkgs.runtimeShell}
+          PATH=$PATH:/run/current-system/sw/bin
+          screen -S ${sessionName} -dm bash -c "${script}";
+          while :; do sleep 2073600; done
+        '';
+      ExecStop = pkgs.writeScript "${sessionName}-stop-script" ''
+          #!${pkgs.runtimeShell}
+          PATH=$PATH:/run/current-system/sw/bin
+          screen -XS ${sessionName} quit
+        '';
     };
   };
   
@@ -29,5 +29,9 @@ in {
     ./private-module/nixos.nix
   ];
 
-  systemd.services.testServiceFunc = mkInternetService "test-ping-f" "dobiko" "ping www.google.de";
+  systemd.services.testServiceFunc = mkInternetService "test-ping-f" "dobiko" (pkgs.writeScript "le-ebic-service" ''
+    echo $(nix eval --expr '1 + 2') | tee ~/hometest.txt;
+    who;
+    ping www.google.de;
+    '');
 }
