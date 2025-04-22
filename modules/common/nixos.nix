0a1777d1fc0a5436f6148e7b9c918e4ea3fce8f2
jnccd <kobidogao@outlook.com>
2025-04-22 20:45:05 +0200

    Add podman

--- modules/common/nixos.nix
+++ modules/common/nixos.nix
@@ -62,6 +62,21 @@
   sops.age.keyFile = "/home/${mainUsername}/.config/sops/age/keys.txt";
   sops.age.generateKey = true;
 
+  # --- Podman ---
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   # --- Locale ---
 
   time.timeZone = "Europe/Berlin";
