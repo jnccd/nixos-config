1ddad98ae4b0e907a608f7f07e65ddad7e0847ab
jnccd <kobidogao@outlook.com>
2025-04-25 18:17:38 +0200

    Fix script

--- copy-dotfiles/from-repo-to-home.sh
+++ copy-dotfiles/from-repo-to-home.sh
@@ -2,4 +2,4 @@
 
 cp -r ~/git/nixos-config/dotfiles/.config/ ~
 cp -r ~/git/nixos-config/dotfiles/.local/ ~
-cp -r ~/git/nixos-config/dotfiles/.background-image/ ~
+cp -r ~/git/nixos-config/dotfiles/.background-image ~
