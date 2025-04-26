5d5dffb6ecf4cac90b350bd077ead3535d02c62f
jnccd <kobidogao@outlook.com>
2025-04-26 16:32:19 +0200

    Fix bash script

--- copy-dotfiles/from-repo-to-home.sh
+++ copy-dotfiles/from-repo-to-home.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp -r ~/git/nixos-config/dotfiles/.config/ ~
-cp -r ~/git/nixos-config/dotfiles/.local/ ~
-cp ~/git/nixos-config/dotfiles/.background-image ~
-cp ~/git/nixos-config/dotfiles/.nanorc ~
+cp -r ./dotfiles/.config/ ~
+cp -r ./dotfiles/.local/ ~
+cp ./dotfiles/.background-image ~
+cp ./dotfiles/.nanorc ~
