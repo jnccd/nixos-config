c937ed85dba1636102839b7617f9322700beb026
jnccd <kobidogao@outlook.com>
2025-04-28 20:00:22 +0200

    Refactor

--- modules/gui/home.nix
+++ modules/gui/home.nix
@@ -11,12 +11,12 @@
     thunderbird
     anki
 
-    (vivaldi.overrideAttrs
-    (oldAttrs: {
-      dontWrapQtApps = false;
-      dontPatchELF = true;
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];
-    }))
+    vivaldi.overrideAttrs
+      (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];
+      })
     vivaldi-ffmpeg-codecs
   ];
 
