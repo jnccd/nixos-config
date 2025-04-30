f56c56e6b826e06c23de54c0c6dc3a2095344b0d
Dobiko <kobidogao@yahoo.com>
2025-04-30 13:39:50 +0200

    Properly use inputs.nixpkgs.follows

--- flake.nix
+++ flake.nix
@@ -17,14 +17,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, nixos-wsl, home-manager, sops-nix, ... }@inputs: let
