c54ca4a7adb64f61c7934a021125dc3afda26378
jnccd <kobidogao@outlook.com>
2025-05-16 00:50:54 +0200

    Add bootloader conf

--- hosts/sv-minis/configuration.nix
+++ hosts/sv-minis/configuration.nix
@@ -10,4 +10,16 @@
     ../../modules/common/nixos.nix
     ../../modules/miniserver
   ];
+
+  # --- Bootloader ---
+
+  boot.loader = {
+    systemd-boot.enable = false;
+    grub.enable = true;
+    grub.device = "nodev";
+    grub.useOSProber = true;
+    grub.efiSupport = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot";
+  };
 }
