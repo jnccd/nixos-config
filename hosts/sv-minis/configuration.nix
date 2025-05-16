ebc2fa28f0f26bd3e3788865006f58f2102d52d0
jnccd <kobidogao@outlook.com>
2025-05-16 03:08:19 +0200

    Simplify bootloader

--- hosts/sv-minis/configuration.nix
+++ hosts/sv-minis/configuration.nix
@@ -12,14 +12,7 @@
   ];
 
   # --- Bootloader ---
-
-  boot.loader = {
-    systemd-boot.enable = false;
-    grub.enable = true;
-    grub.device = "nodev";
-    grub.useOSProber = true;
-    grub.efiSupport = true;
-    efi.canTouchEfiVariables = true;
-    efi.efiSysMountPoint = "/boot";
-  };
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
