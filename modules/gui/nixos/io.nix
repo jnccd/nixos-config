8b9723890ea9015aa497c339da6d4f0bb5fa5770
jnccd <kobidogao@outlook.com>
2025-05-16 18:04:14 +0200

    Remove keymap config

--- modules/gui/nixos/io.nix
+++ modules/gui/nixos/io.nix
@@ -1,11 +1,4 @@
 { config, lib, pkgs, ... }: {
-  # Configure keymap
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
-  console.keyMap = "de";
-
   services.printing.enable = true;
 
   hardware.pulseaudio.enable = false;
