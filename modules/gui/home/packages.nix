e27874ce591d77f0aff3986a7351db72b3b4ffd2
jnccd <kobidogao@outlook.com>
2025-05-17 15:51:02 +0200

    Refactor

--- modules/gui/home/packages.nix
+++ modules/gui/home/packages.nix
@@ -1,19 +1,18 @@
 { config, pkgs, globalArgs, ... }: {
-  home.packages = with pkgs; [
-    kdePackages.filelight
-    kdePackages.kate
-    gparted
+  home.packages = with pkgs;
+    [
+      gparted
 
-    gimp
-    thunderbird
-    anki
+      gimp
+      thunderbird
+      anki
 
-    (vivaldi.overrideAttrs (oldAttrs: {
-      dontWrapQtApps = false;
-      dontPatchELF = true;
-      nativeBuildInputs = oldAttrs.nativeBuildInputs
-        ++ [ pkgs.kdePackages.wrapQtAppsHook ];
-    }))
-    vivaldi-ffmpeg-codecs
-  ];
+      (vivaldi.overrideAttrs (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs
+          ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+      }))
+      vivaldi-ffmpeg-codecs
+    ] ++ (with kdePackages; [ filelight kate ]);
 }
