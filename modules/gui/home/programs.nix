54efccd9a40d42d8ea8a8bcca1080da9c7775255
jnccd <kobidogao@outlook.com>
2025-05-17 15:24:54 +0200

    Disable vscode.profiles.default.userSettings

--- modules/gui/home/programs.nix
+++ modules/gui/home/programs.nix
@@ -16,42 +16,42 @@
       ms-vscode-remote.remote-ssh
     ];
 
-    profiles.default.userSettings = {
-      "git.autofetch" = true;
-      "latex-workshop.view.pdf.viewer" = "tab";
-      "git.confirmSync" = false;
-      "git.enableSmartCommit" = true;
-      "[latex]" = { "editor.defaultFormatter" = "mathematic.vscode-latex"; };
-      "terminal.integrated.persistentSessionScrollback" = 1000;
-      "terminal.integrated.scrollback" = 100000;
-      "latex.formatter.columnLimit" = 600;
-      "latex-workshop.message.latexlog.exclude" =
-        [ ".*ChkTeX does not handle lines over.*" ];
-      "cSpell.customDictionaries" = { };
-      "cSpell.maxNumberOfProblems" = 500;
-      "cSpell.maxDuplicateProblems" = 25;
-      "cSpell.numSuggestions" = 24;
-      "cSpell.dictionaryDefinitions" = [ ];
-      "cSpell.language" = "en;de";
-      "editor.formatOnSave" = true;
-      "workbench.iconTheme" = "material-icon-theme";
-      "[typescriptreact]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "workbench.colorTheme" = "Default Dark+";
-      "diffEditor.ignoreTrimWhitespace" = false;
-      "csharp.experimental.debug.hotReload" = true;
-      "csharp.debug.hotReloadVerbosity" = "detailed";
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings" = {
-        "nil" = {
-          "diagnostics" = { "ignored" = [ "unused_binding" ]; };
-          "formatting" = { "command" = [ "nixfmt" ]; };
-          "nix" = { "flake" = { "autoEvalInputs" = true; }; };
-        };
-      };
-      "nix.formatterPath" = "nixfmt";
-    };
+    # profiles.default.userSettings = {
+    #   "git.autofetch" = true;
+    #   "latex-workshop.view.pdf.viewer" = "tab";
+    #   "git.confirmSync" = false;
+    #   "git.enableSmartCommit" = true;
+    #   "[latex]" = { "editor.defaultFormatter" = "mathematic.vscode-latex"; };
+    #   "terminal.integrated.persistentSessionScrollback" = 1000;
+    #   "terminal.integrated.scrollback" = 100000;
+    #   "latex.formatter.columnLimit" = 600;
+    #   "latex-workshop.message.latexlog.exclude" =
+    #     [ ".*ChkTeX does not handle lines over.*" ];
+    #   "cSpell.customDictionaries" = { };
+    #   "cSpell.maxNumberOfProblems" = 500;
+    #   "cSpell.maxDuplicateProblems" = 25;
+    #   "cSpell.numSuggestions" = 24;
+    #   "cSpell.dictionaryDefinitions" = [ ];
+    #   "cSpell.language" = "en;de";
+    #   "editor.formatOnSave" = true;
+    #   "workbench.iconTheme" = "material-icon-theme";
+    #   "[typescriptreact]" = {
+    #     "editor.defaultFormatter" = "esbenp.prettier-vscode";
+    #   };
+    #   "workbench.colorTheme" = "Default Dark+";
+    #   "diffEditor.ignoreTrimWhitespace" = false;
+    #   "csharp.experimental.debug.hotReload" = true;
+    #   "csharp.debug.hotReloadVerbosity" = "detailed";
+    #   "nix.enableLanguageServer" = true;
+    #   "nix.serverPath" = "nil";
+    #   "nix.serverSettings" = {
+    #     "nil" = {
+    #       "diagnostics" = { "ignored" = [ "unused_binding" ]; };
+    #       "formatting" = { "command" = [ "nixfmt" ]; };
+    #       "nix" = { "flake" = { "autoEvalInputs" = true; }; };
+    #     };
+    #   };
+    #   "nix.formatterPath" = "nixfmt";
+    # };
   };
 }
