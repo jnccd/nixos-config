392630e7e847afe7c4c7caa107473ec3d30d6eef
jnccd <kobidogao@outlook.com>
2025-05-22 22:43:21 +0200

    Add git-pull alias again

--- modules/common/nixos/misc.nix
+++ modules/common/nixos/misc.nix
@@ -28,6 +28,7 @@
   environment.shellAliases = {
     owo = "echo uwu"; # I owo into the void and the void uwus back
     "cd.." = "cd ..";
+    git-pull = "git pull --recurse-submodules";
 
     nix-rb =
       "sudo nixos-rebuild switch --flake /home/${globalArgs.mainUsername}/git/nixos-config?submodules=1 && home-manager switch -b backup --flake /home/${globalArgs.mainUsername}/git/nixos-config?submodules=1 && bash /home/${globalArgs.mainUsername}/git/nixos-config/copy-dotfiles/from-repo-to-home.sh";
