8a2cba417e46d00879c00cd8714ff07965c77dab
jnccd <kobidogao@outlook.com>
2025-05-23 20:21:23 +0200

    Apply environment.variables again

--- modules/gui/nixos/csharp-development.nix
+++ modules/gui/nixos/csharp-development.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ dotnet-sdk dotnet-ef ];
 
-  environment.variables = { DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = 1; };
-  environment.sessionVariables = { DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = 1; };
+  environment.variables = { DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = "1"; };
 }
