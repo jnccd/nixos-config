f1b841982f6c38e30bc05c217b98b45250611909
jnccd <kobidogao@outlook.com>
2025-05-23 15:47:28 +0200

    Update aliases

--- modules/common/nixos/aliases.nix
+++ modules/common/nixos/aliases.nix
@@ -1,13 +1,19 @@
-{ config, lib, pkgs, globalArgs, ... }: {
+{ config, lib, pkgs, globalArgs, ... }:
+let nixosConfigPath = "/home/${globalArgs.mainUsername}/git/nixos-config";
+in {
   environment.shellAliases = {
     owo = "echo uwu"; # I owo into the void and the void uwus back
     "cd.." = "cd ..";
     git-pull =
       "git pull && git submodule foreach 'git checkout main || git checkout -b main origin/main && git pull origin main'";
+    git-pull-nixconf = pkgs.writeScript "git-pull-nixconf" ''
+      cd ${nixosConfigPath}
+      git-pull
+    '';
 
     nix-rb =
-      "sudo nixos-rebuild switch --flake /home/${globalArgs.mainUsername}/git/nixos-config?submodules=1 && home-manager switch -b backup --flake /home/${globalArgs.mainUsername}/git/nixos-config?submodules=1 && bash /home/${globalArgs.mainUsername}/git/nixos-config/copy-dotfiles/from-repo-to-home.sh";
-    nix-prb = "git-pull && nix-rb";
+      "sudo nixos-rebuild switch --flake ${nixosConfigPath}?submodules=1 && home-manager switch -b backup --flake ${nixosConfigPath}?submodules=1 && bash ${nixosConfigPath}/copy-dotfiles/from-repo-to-home.sh";
+    nix-prb = "git-pull-nixconf && nix-rb";
     nix-gc = "nix-collect-garbage -d";
   };
 }
