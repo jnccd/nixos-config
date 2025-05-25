4a9f26823ccca983434fc104653101de48b30463
jnccd <kobidogao@outlook.com>
2025-05-25 19:38:32 +0200

    Just dump everything in default in home manager folders

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -1,7 +1,31 @@
 { config, pkgs, globalArgs, ... }: {
-  imports = [
-    ./misc.nix
-    ./programs.nix
 
-  ];
+  # --- Nix ---
+
+  nixpkgs.config.allowUnfree = true;
+
+  # --- Main User ---
+
+  home = {
+    username = globalArgs.mainUsername;
+    homeDirectory = "/home/${globalArgs.mainUsername}";
+    stateVersion = globalArgs.homeStateVersion;
+  };
+
+  # --- Programs ---
+
+  programs.git = {
+    enable = true;
+    userName = globalArgs.githubUsername;
+    userEmail = globalArgs.email;
+  };
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+    bash.enable = true;
+  };
 }
--- modules/gui/home/default.nix
+++ modules/gui/home/default.nix
@@ -1,6 +1,27 @@
 { config, pkgs, globalArgs, ... }: {
-  imports = [
-    ./programs.nix
 
-  ];
+  # --- Programs ---
+
+  home.packages = with pkgs; [ dotnet-sdk dotnet-ef ]; # For C# dev
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      jnoortheen.nix-ide
+      arrterian.nix-env-selector
+      signageos.signageos-vscode-sops
+
+      pkief.material-icon-theme
+      esbenp.prettier-vscode
+
+      streetsidesoftware.code-spell-checker
+
+      ms-dotnettools.csdevkit
+      ms-python.python
+
+      ms-vscode-remote.remote-ssh
+    ];
+  };
 }
