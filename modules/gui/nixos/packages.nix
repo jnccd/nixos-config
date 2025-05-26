c458fdb75e030512a113207c2575359443930d28
jnccd <kobidogao@outlook.com>
2025-05-26 15:49:31 +0200

    Refactor

--- modules/gui/nixos/packages.nix
+++ modules/gui/nixos/packages.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = (with pkgs; [
     # Base
     plasma-panel-colorizer
+    kdePackages.qtmultimedia # For sddm theme
     (sddm-astronaut.override {
       embeddedTheme = "purple_leaves";
       themeConfig = { FormPosition = "left"; };
@@ -13,7 +14,7 @@
       dontWrapQtApps = false;
       dontPatchELF = true;
       nativeBuildInputs = oldAttrs.nativeBuildInputs
-        ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+        ++ [ kdePackages.wrapQtAppsHook ];
     }))
     vivaldi-ffmpeg-codecs
 
@@ -25,6 +26,8 @@
     gparted
     alsa-utils
     pgadmin4-desktopmode
+    kdePackages.filelight
+    kdePackages.kate
 
     # Productivity
     libreoffice-qt6-fresh
@@ -32,9 +35,5 @@
     anki
 
     plemoljp-nf # Neovim fonts
-  ]) ++ (with pkgs.kdePackages; [
-    qtmultimedia # For sddm theme
-    filelight
-    kate
   ]);
 }
