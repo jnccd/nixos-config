ac58a514801aa74d969d7dbb4817a34df45bda1b
jnccd <kobidogao@outlook.com>
2025-05-26 15:16:27 +0200

    Format

--- modules/gui/nixos/packages.nix
+++ modules/gui/nixos/packages.nix
@@ -1,42 +1,39 @@
 { config, lib, pkgs, ... }: {
-  environment.systemPackages = (with pkgs;
-    [
-      # Base
-      plasma-panel-colorizer
-      (sddm-astronaut.override {
-        embeddedTheme = "purple_leaves";
-        themeConfig = {
-          FormPosition="left";
-        };
-      })
+  environment.systemPackages = (with pkgs; [
+    # Base
+    plasma-panel-colorizer
+    (sddm-astronaut.override {
+      embeddedTheme = "purple_leaves";
+      themeConfig = { FormPosition = "left"; };
+    })
 
-      # Browser
-      firefox
-      (vivaldi.overrideAttrs (oldAttrs: {
-        dontWrapQtApps = false;
-        dontPatchELF = true;
-        nativeBuildInputs = oldAttrs.nativeBuildInputs
-          ++ [ pkgs.kdePackages.wrapQtAppsHook ];
-      }))
-      vivaldi-ffmpeg-codecs
+    # Browser
+    firefox
+    (vivaldi.overrideAttrs (oldAttrs: {
+      dontWrapQtApps = false;
+      dontPatchELF = true;
+      nativeBuildInputs = oldAttrs.nativeBuildInputs
+        ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+    }))
+    vivaldi-ffmpeg-codecs
 
-      # Multimedia
-      vlc
-      gimp
+    # Multimedia
+    vlc
+    gimp
 
-      # Tools
-      gparted
-      alsa-utils
-      pgadmin4-desktopmode
+    # Tools
+    gparted
+    alsa-utils
+    pgadmin4-desktopmode
 
-      # Productivity
-      thunderbird
-      anki
+    # Productivity
+    thunderbird
+    anki
 
-      plemoljp-nf # Neovim fonts
-    ]) ++ (with pkgs.kdePackages; [
-      qtmultimedia # For sddm theme
-      filelight 
-      kate 
-    ]);
+    plemoljp-nf # Neovim fonts
+  ]) ++ (with pkgs.kdePackages; [
+    qtmultimedia # For sddm theme
+    filelight
+    kate
+  ]);
 }
