33c54cc07e84cba2702519a263db672c80f02b62
jnccd <kobidogao@outlook.com>
2025-06-10 11:16:21 +0200

    Use new recommended nvidia driver settings

--- hosts/pc-ryzen/configuration.nix
+++ hosts/pc-ryzen/configuration.nix
@@ -17,13 +17,15 @@
 
   # --- NVidia ---
 
+  hardware.graphics = { enable = true; };
+
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
