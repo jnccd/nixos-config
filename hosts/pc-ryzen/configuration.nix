09b308db4aee522fe446fd57da0974fd536e9d11
jnccd <kobidogao@outlook.com>
2025-06-10 10:40:35 +0200

    Try to fix sleep issues

--- hosts/pc-ryzen/configuration.nix
+++ hosts/pc-ryzen/configuration.nix
@@ -21,7 +21,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
