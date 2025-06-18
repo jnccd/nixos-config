aaf19d1a88eb31ce487c4bab9385878466d922c5
jnccd <kobidogao@outlook.com>
2025-06-18 09:55:27 +0200

    Clean up flake

---
+++ commands.sh
@@ -0,0 +1,23 @@
+exit 0 # Dont execute this
+
+# Standard Nix Header
+#{ config, lib, pkgs, globalArgs, ... }:
+
+sudo nixos-rebuild switch --flake .?submodules=1
+sudo nixos-rebuild switch --install-bootloader --flake .?submodules=1
+nix flake update --flake .?submodules=1
+home-manager switch -b backup --flake .?submodules=1
+nix-collect-garbage -d
+
+sudo nixos-rebuild switch --flake .?submodules=1 && home-manager switch -b backup --flake .?submodules=1
+
+sops -e -i secrets/
+sops secrets/
+sops updatekeys secrets/
+nix-shell -p ssh-to-age --run 'cat ~/.ssh/id_ed25519.pub | ssh-to-age'
+nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/keys.txt" && sudo mv ~/keys.txt ~/.config/sops/age/keys.txt
+cat ~/.ssh/id_ed25519.pub | ssh dobiko@minis "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+
+git grep "secret" $(git rev-list --all)
+git submodule update --init --recursive
+git submodule update --recursive --remote
--- flake.nix
+++ flake.nix
@@ -1,23 +1,3 @@
-# sudo nixos-rebuild switch --flake .?submodules=1
-# sudo nixos-rebuild switch --install-bootloader --flake .?submodules=1
-# nix flake update --flake .?submodules=1
-# home-manager switch -b backup --flake .?submodules=1
-# nix-collect-garbage -d
-# ---
-# sudo nixos-rebuild switch --flake .?submodules=1 && home-manager switch -b backup --flake .?submodules=1
-# ---
-# sops -e -i secrets/
-# sops secrets/
-# sops updatekeys secrets/
-# nix-shell -p ssh-to-age --run 'cat ~/.ssh/id_ed25519.pub | ssh-to-age'
-# nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/keys.txt" && sudo mv ~/keys.txt ~/.config/sops/age/keys.txt
-# cat ~/.ssh/id_ed25519.pub | ssh dobiko@minis "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-# ---
-# git grep "secret" $(git rev-list --all)
-# git submodule update --init --recursive
-# git submodule update --recursive --remote
-# ---
-# { config, lib, pkgs, globalArgs, ... }:
 {
   description = "Dobiko Config";
 
@@ -51,6 +31,11 @@
 
         defaultSystemUsername = "runner";
       };
+
+      hostsDir = ./hosts;
+      entries = builtins.readDir hostsDir;
+      hostNames = builtins.attrNames
+        (builtins.filterAttrs (name: type: type == "directory") entries);
 
       hosts = [
         {
