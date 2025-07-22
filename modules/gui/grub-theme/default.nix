903ba3018733c9fde292f58fd8a3e36808e7ca4d
jnccd <kobidogao@outlook.com>
2025-07-22 15:48:11 +0200

    Simplify grub theme

--- modules/gui/grub-theme/default.nix
+++ modules/gui/grub-theme/default.nix
@@ -1,12 +1,5 @@
-{ config, lib, pkgs, globalArgs, ... }:
-let
-  crossgrub-src = pkgs.fetchFromGitHub {
-    owner = "krypciak";
-    repo = "crossgrub";
-    tag = "1.0.0";
-    hash = "sha256-TDgi9e2/aHngdzFCkx0ykZedP3v4IFKiYJGTcWUo+rk=";
-  };
-in {
+{ config, lib, pkgs, globalArgs, ... }: {
   boot.loader.grub.theme = "${import ./crossgrub.nix { inherit pkgs lib; }}";
-  boot.loader.grub.splashImage = "${crossgrub-src}/assets/background.png";
+  boot.loader.grub.splashImage =
+    "${import ./crossgrub.nix { inherit pkgs lib; }}/background.png";
 }
