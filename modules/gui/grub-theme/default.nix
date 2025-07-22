fadcddd4b4c9f33bdfaf79fd21dc847ab20a1c85
jnccd <kobidogao@outlook.com>
2025-07-22 15:07:52 +0200

    Try to set the splashImage too

--- modules/gui/grub-theme/default.nix
+++ modules/gui/grub-theme/default.nix
@@ -1,3 +1,12 @@
-{ config, lib, pkgs, globalArgs, ... }: {
+{ config, lib, pkgs, globalArgs, ... }:
+let
+  crossgrub-src = pkgs.fetchFromGitHub {
+    owner = "krypciak";
+    repo = "crossgrub";
+    tag = "1.0.0";
+    hash = "sha256-TDgi9e2/aHngdzFCkx0ykZedP3v4IFKiYJGTcWUo+rk=";
+  };
+in {
   boot.loader.grub.theme = "${import ./crossgrub.nix { inherit pkgs lib; }}";
+  boot.loader.grub.splashImage = "${crossgrub-src}/assets/background.png";
 }
