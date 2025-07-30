79805c227758d2523f561e92fc45e661373844b6
jnccd <kobidogao@outlook.com>
2025-07-30 09:35:32 +0200

    Remove xsetupScript

--- modules/generic/gui/ui.nix
+++ modules/generic/gui/ui.nix
@@ -18,26 +18,6 @@
         ((builtins.genList (x: "nixbld" + (builtins.toString x)) 33)
           ++ [ globalArgs.defaultSystemUsername ]));
     };
-    # Only use the first connected display
-    xsetupScript = ''
-      #!/bin/sh
-      export PATH=${pkgs.xorg.xrandr}/bin:$PATH
-
-      # Get connected displays
-      connected_displays=$(xrandr | grep " connected" | cut -d' ' -f1)
-
-      # Choose the first one as primary
-      primary=$(echo "$connected_displays" | head -n 1)
-
-      # Turn off all other displays
-      for display in $connected_displays; do
-        if [ "$display" = "$primary" ]; then
-          xrandr --output "$display" --auto --primary
-        else
-          xrandr --output "$display" --off
-        fi
-      done
-    '';
     theme = "sddm-astronaut-theme";
   };
 }
