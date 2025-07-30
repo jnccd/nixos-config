072a75cdbb1101f82e164938c613346d6dc2fb87
jnccd <kobidogao@outlook.com>
2025-07-30 09:53:32 +0200

    Try to configure SDDM via X11.DisplayCommand

--- modules/generic/gui/ui.nix
+++ modules/generic/gui/ui.nix
@@ -17,7 +17,28 @@
       Users.HideUsers = (lib.lists.foldl (a: b: a + "," + b) ""
         ((builtins.genList (x: "nixbld" + (builtins.toString x)) 33)
           ++ [ globalArgs.defaultSystemUsername ]));
+      X11.DisplayCommand = "/etc/sddm/scripts/my-xsetup";
     };
     theme = "sddm-astronaut-theme";
   };
+
+  # Only use the first connected display
+  environment.etc."sddm/scripts/my-xsetup".text = ''
+    #!/bin/sh
+    export PATH=${pkgs.xorg.xrandr}/bin:$PATH
+
+    connected_displays=$(xrandr | grep " connected" | cut -d' ' -f1)
+    primary=$(echo "$connected_displays" | grep -E '^DP' | head -n 1)
+    if [ -z "$primary" ]; then
+      primary=$(echo "$connected_displays" | head -n 1)
+    fi
+
+    for display in $connected_displays; do
+      if [ "$display" = "$primary" ]; then
+        xrandr --output "$display" --auto --primary
+      else
+        xrandr --output "$display" --off
+      fi
+    done
+  '';
 }
