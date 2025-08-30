6655c4b1967ad66d1b3212bd96d3715494514f07
jnccd <kobidogao@outlook.com>
2025-08-30 19:25:19 +0200

    Add nas mounting service

--- commands.sh
+++ commands.sh
@@ -23,6 +23,7 @@ nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/
 cat ~/.ssh/id_ed25519.pub | ssh dobiko@minis "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 
 git grep "secret" $(git rev-list --all)
+git submodule add git@github.com:jnccd/nixos-config-common-private-module.git ./modules/generic/common/private-module
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
