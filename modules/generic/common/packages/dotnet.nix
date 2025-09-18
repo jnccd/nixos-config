a0d10b9d025f43620ad56380ded02c9c245e7ef5
jnccd <kobidogao@outlook.com>
2025-09-18 14:15:45 +0200

    Disable dotnet telemetry systemwide

--- modules/generic/common/packages/dotnet.nix
+++ modules/generic/common/packages/dotnet.nix
@@ -6,6 +6,10 @@
   };
 
   config = lib.mkIf config.dobikoConf.dotnet.enabled {
+    environment.variables = {
+      DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+      DOTNET_NOLOGO = "true";
+    };
     environment.systemPackages = with pkgs; [
       dotnet-sdk
       dotnet-ef
