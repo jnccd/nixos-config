08e08ac7d5fd727ec4d9dc53531d4d25a6e7ec8c
jnccd <kobidogao@outlook.com>
2025-10-10 13:20:28 +0200

    Make printing toggleable

--- modules/generic/gui/printer.nix
+++ modules/generic/gui/printer.nix
@@ -1,35 +1,43 @@
 { config, lib, pkgs, ... }: {
-  # Print
-  services.printing = {
-    enable = true;
-    browsing = true;
-    defaultShared = true;
-    drivers = with pkgs; [
-      cups-filters
-      gutenprint
-      gutenprintBin
-      brlaser
-      brgenml1lpr
-      brgenml1cupswrapper
+  options.dobikoConf.printing.enabled = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enables printing services";
+  };
+
+  config = lib.mkIf config.dobikoConf.printing.enabled {
+    # Print
+    services.printing = {
+      enable = true;
+      browsing = true;
+      defaultShared = true;
+      drivers = with pkgs; [
+        cups-filters
+        gutenprint
+        gutenprintBin
+        brlaser
+        brgenml1lpr
+        brgenml1cupswrapper
+      ];
+    };
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    services.ipp-usb.enable = true;
+    # Sometimes it is necessary to use: sudo cupsenable <printer_name>; sudo cupsaccept <printer_name>
+    # Often it works best to register the printer in http://localhost:631 manually
+
+    # Scan
+    hardware.sane.enable = true;
+    hardware.sane.extraBackends = with pkgs; [ brscan4 sane-airscan ];
+    hardware.sane.brscan4.enable = true;
+    services.udev.packages = [ pkgs.sane-airscan ];
+    environment.systemPackages = with pkgs; [
+      brscan4
+      sane-backends
+      kdePackages.skanlite
     ];
   };
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-  services.ipp-usb.enable = true;
-  # Sometimes it is necessary to use: sudo cupsenable <printer_name>; sudo cupsaccept <printer_name>
-  # Often it works best to register the printer in http://localhost:631 manually
-
-  # Scan
-  hardware.sane.enable = true;
-  hardware.sane.extraBackends = with pkgs; [ brscan4 sane-airscan ];
-  hardware.sane.brscan4.enable = true;
-  services.udev.packages = [ pkgs.sane-airscan ];
-  environment.systemPackages = with pkgs; [
-    brscan4
-    sane-backends
-    kdePackages.skanlite
-  ];
 }
