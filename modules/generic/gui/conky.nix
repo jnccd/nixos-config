78fe6c7dceb8b4a548f2bdc89b5882c9bed95f67
jnccd <kobidogao@outlook.com>
2025-10-11 21:54:11 +0200

    Add conky option

--- modules/generic/gui/conky.nix
+++ modules/generic/gui/conky.nix
@@ -1,25 +1,33 @@
 { config, lib, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    conky
-    lm_sensors
-    sysstat
-    intel-gpu-tools
-
-  ];
-
-  security.wrappers.intel_gpu_top = {
-    source = "${pkgs.intel-gpu-tools}/bin/intel_gpu_top";
-    capabilities = "cap_perfmon+ep";
-    owner = "root";
-    group = "root";
-    permissions = "0755";
+  options.dobikoConf.conky.enabled = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Shows hardware monitoring info in a compact widget";
   };
 
-  environment.etc."xdg/autostart/conky.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=Conky
-    Exec=conky -c ~/.config/conky/conky.conf
-    X-GNOME-Autostart-enabled=true
-  '';
+  config = lib.mkIf config.dobikoConf.conky.enabled {
+    environment.systemPackages = with pkgs; [
+      conky
+      lm_sensors
+      sysstat
+      intel-gpu-tools
+
+    ];
+
+    security.wrappers.intel_gpu_top = {
+      source = "${pkgs.intel-gpu-tools}/bin/intel_gpu_top";
+      capabilities = "cap_perfmon+ep";
+      owner = "root";
+      group = "root";
+      permissions = "0755";
+    };
+
+    environment.etc."xdg/autostart/conky.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Conky
+      Exec=conky -c ~/.config/conky/conky.conf
+      X-GNOME-Autostart-enabled=true
+    '';
+  };
 }
