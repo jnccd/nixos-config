d12444fcf23f5d9e86b2e06dabb6f1c257e285a0
jnccd <kobidogao@outlook.com>
2025-10-11 21:50:44 +0200

    Filter first iostat out

--- dotfiles/.config/conky/disk_io.sh
+++ dotfiles/.config/conky/disk_io.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-iostat -dx 1 2 | awk 'NF && $1 !~ /^(Device|Linux)/ {print $1, $3"Br", $9"Bw", $23"%"}'
+iostat -dx 1 2 | awk 'NF && $1 !~ /^(Device|Linux)/ {print $1, $3"Br", $9"Bw", $23"%"}' | awk '{ a[$1]=$0 } END { for (i in a) print a[i] }'
