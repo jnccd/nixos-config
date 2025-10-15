06104bae181f15ed8ce4f01bf4ba0ae797804b47
jnccd <kobidogao@outlook.com>
2025-10-15 17:03:31 +0200

    Tune postgres

--- modules/generic/common/postgres.nix
+++ modules/generic/common/postgres.nix
@@ -13,6 +13,27 @@
     services.postgresql = {
       enable = true;
       package = pkgs.postgresql_16;
+
+      # Tuned for SSDs using https://pgtune.leopard.in.ua
+      settings = {
+        max_connections = 200;
+        shared_buffers = "512MB";
+        effective_cache_size = "1536MB";
+        maintenance_work_mem = "128MB";
+        checkpoint_completion_target = 0.9;
+        wal_buffers = "16MB";
+        default_statistics_target = 100;
+        random_page_cost = 1.1;
+        effective_io_concurrency = 200;
+        work_mem = "2570kB";
+        huge_pages = "off";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = 4;
+        max_parallel_workers_per_gather = 2;
+        max_parallel_workers = 4;
+        max_parallel_maintenance_workers = 2;
+      };
     };
     systemd.services = lib.custom.mkWrappedScreenService {
       sessionName = "psql-init";
