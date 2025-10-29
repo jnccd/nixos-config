b005462d202ad29bab1964c2f73d5d2820886960
jnccd <kobidogao@outlook.com>
2025-10-29 09:57:05 +0100

    Really disable NIXOS_OZONE_WL

--- modules/generic/gui/desktop-env/misc.nix
+++ modules/generic/gui/desktop-env/misc.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }: {
   programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "0";
+  environment.sessionVariables.NIXOS_OZONE_WL = "";
 }
