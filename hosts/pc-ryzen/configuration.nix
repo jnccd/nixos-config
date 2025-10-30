51a5b96be94f702b78acc1011821dc2a93622e4a
jnccd <kobidogao@outlook.com>
2025-10-30 10:50:12 +0100

    Remove fcitx5 config

--- hosts/pc-ryzen/configuration.nix
+++ hosts/pc-ryzen/configuration.nix
@@ -13,8 +13,6 @@
 
   dobikoConf.nvidia.enabled = true;
   dobikoConf.gaming.enabled = true;
-  dobikoConf.fcitx5.layout = "us(altgr-intl)";
-  dobikoConf.fcitx5.im = "keyboard-us-altgr-intl";
 
   # --- Misc ---
 
--- modules/generic/gui/keyboard.nix
+++ modules/generic/gui/keyboard.nix
@@ -1,72 +1,7 @@
 { inputs, config, lib, pkgs, globalArgs, ... }: {
-  options.dobikoConf.fcitx5.layout = lib.mkOption {
-    type = lib.types.str;
-    default = "de";
-    description = "Keyboard layout used in fcitx5";
-  };
-  options.dobikoConf.fcitx5.im = lib.mkOption {
-    type = lib.types.str;
-    default = "keyboard-de";
-    description = "Keyboard input method used in fcitx5";
-  };
-
-  config = {
-    services.xserver.xkb = lib.mkDefault {
-      layout = "de,us";
-      variant = ",altgr-intl";
-      options = "grp:win_space_toggle";
-    };
-    # i18n.inputMethod = lib.mkDefault {
-    #   enable = true;
-    #   type = "fcitx5";
-    #   fcitx5 = {
-    #     waylandFrontend = true;
-    #     plasma6Support = true;
-    #     ignoreUserConfig = true;
-    #     addons = with pkgs; [
-    #       fcitx5-configtool
-    #       fcitx5-chinese-addons
-    #       fcitx5-mozc
-    #     ];
-    #     settings.inputMethod = {
-    #       "Groups/0" = {
-    #         "Name" = "Default";
-    #         "Default Layout" = config.dobikoConf.fcitx5.layout;
-    #         "DefaultIM" = config.dobikoConf.fcitx5.im;
-    #       };
-    #       "Groups/0/Items/0" = {
-    #         "Name" = config.dobikoConf.fcitx5.im;
-    #         "Layout" = config.dobikoConf.fcitx5.layout;
-    #       };
-    #       "Groups/0/Items/1" = {
-    #         "Name" =
-    #           if config.dobikoConf.fcitx5.im == "keyboard-us-altgr-intl" then
-    #             "keyboard-de"
-    #           else
-    #             "keyboard-us-altgr-intl";
-    #         "Layout" = if config.dobikoConf.fcitx5.im == "us(altgr-intl)" then
-    #           "de"
-    #         else
-    #           "us(altgr-intl)";
-    #       };
-    #       "Groups/0/Items/2" = {
-    #         "Name" = "pinyin";
-    #         "Layout" = "";
-    #       };
-    #       "Groups/0/Items/3" = {
-    #         "Name" = "mozc";
-    #         "Layout" = "";
-    #       };
-    #       "GroupOrder" = { "0" = "Default"; };
-    #     };
-    #   };
-    # };
-    # environment.etc."xdg/fcitx5/conf/keyboard.conf".text =
-    #   "EnableHintByDefault=False";
-    # environment.sessionVariables = {
-    #   XMODIFIERS = "@im=fcitx";
-    #   GTK_IM_MODULE = "fcitx";
-    #   QT_IM_MODULE = "fcitx";
-    # };
+  services.xserver.xkb = lib.mkDefault {
+    layout = "de,us";
+    variant = ",altgr-intl";
+    options = "grp:win_space_toggle";
   };
 }
