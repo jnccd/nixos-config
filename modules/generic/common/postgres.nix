981603c7c241ee44d81ae17291725fccb10bfc42
jnccd <kobidogao@outlook.com>
2026-01-02 16:02:50 +0100

    Lower postgres resource usage

--- modules/generic/common/postgres.nix
+++ modules/generic/common/postgres.nix
@@ -16,16 +16,16 @@
 
       # Tuned for SSDs using https://pgtune.leopard.in.ua
       settings = {
-        max_connections = 200;
-        shared_buffers = "512MB";
-        effective_cache_size = "1536MB";
-        maintenance_work_mem = "128MB";
+        max_connections = 50;
+        shared_buffers = "256MB";
+        effective_cache_size = "768MB";
+        maintenance_work_mem = "64MB";
         checkpoint_completion_target = 0.9;
-        wal_buffers = "16MB";
+        wal_buffers = "7864kB";
         default_statistics_target = 100;
         random_page_cost = 1.1;
         effective_io_concurrency = 200;
-        work_mem = "2570kB";
+        work_mem = "4854kB";
         huge_pages = "off";
         min_wal_size = "1GB";
         max_wal_size = "4GB";
