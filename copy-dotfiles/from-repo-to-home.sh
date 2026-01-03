c9aaa0c09bbb94b4b68928ac5dfa7d1532e564f0
jnccd <kobidogao@outlook.com>
2026-01-03 23:04:57 +0100

    Improve checks

--- copy-dotfiles/from-repo-to-home.sh
+++ copy-dotfiles/from-repo-to-home.sh
@@ -3,17 +3,23 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-rsync -av ../dotfiles/ ~/
-
 if [[ -n "$COPY_DOTFILES_SUDOLESS" ]]; then
-  exit 0
+  rsync -av ../dotfiles/ ~/
+else
+    # Propagate terminal settings to all users
+    while IFS=: read -r user _ uid _ _ home shell; do
+    if [ "$home" != "/var/empty" ] && [ "$home" != "/run/dbus" ] && [ "$home" != "/var/lib/davis" ] && [ -d "$home" ]; then
+        echo "$home"
+        
+        if [ "$uid" -ge 1000 ]; then
+            # Real users get everything
+            sudo rsync -av "../dotfiles/" "$home/"
+        else
+            # System users get terminal settings
+            sudo rsync -av "../dotfiles/.config/nushell/" "$home/.config/nushell/"
+            sudo rsync -av "../dotfiles/.config/nvim/" "$home/.config/nvim/"
+        fi
+    fi
+    done < /etc/passwd
 fi
 
-# Propagate terminal settings to all users
-while IFS=: read -r user _ uid _ _ home shell; do
-  if [ "$home" != "/var/empty" ] && [ -d "$home" ]; then
-    echo "$home"
-    sudo rsync -av "../dotfiles/.config/nushell/" "$home/.config/nushell/"
-    sudo rsync -av "../dotfiles/.config/nvim/" "$home/.config/nvim/"
-  fi
-done < /etc/passwd
