7687fafbf7cc7039d22b1c580fec0ed3bb566960
jnccd <kobidogao@outlook.com>
2026-01-06 14:22:51 +0100

    Move direnv to nixosConf

--- modules/homemanager/common/default.nix
+++ modules/homemanager/common/default.nix
@@ -20,13 +20,4 @@
       Email = homeUser.email;
     };
   };
-
-  programs = {
-    direnv = {
-      enable = true;
-      enableBashIntegration = true;
-      nix-direnv.enable = true;
-    };
-    bash.enable = true;
-  };
 }
