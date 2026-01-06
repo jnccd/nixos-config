45c2bd9c7fe72f4d31960365976e8074178dc9aa
jnccd <kobidogao@outlook.com>
2026-01-06 10:21:41 +0100

    Chown correctly

--- copy-dotfiles/from-repo-to-home.sh
+++ copy-dotfiles/from-repo-to-home.sh
@@ -7,7 +7,7 @@ if [[ -n "$COPY_DOTFILES_SUDOLESS" ]]; then
   rsync -av ../dotfiles/ ~/
 else
     # Propagate terminal settings to all users
-    while IFS=: read -r user _ uid _ _ home shell; do
+    while IFS=: read -r user _ uid gid _ home shell; do
     if [ "$home" != "/var/empty" ] && [ "$home" != "/run/dbus" ] && [ "$home" != "/var/lib/davis" ] && [ -d "$home" ]; then
         echo "$home"
         
@@ -19,6 +19,8 @@ else
             sudo rsync -av "../dotfiles/.config/nushell/" "$home/.config/nushell/"
             sudo rsync -av "../dotfiles/.config/nvim/" "$home/.config/nvim/"
         fi
+
+        sudo chown $uid:$gid "$home/"
     fi
     done < /etc/passwd
 fi
