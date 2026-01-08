af010f6cba41c54bc9d4800dea7113517ad2ca67
jnccd <kobidogao@outlook.com>
2026-01-08 11:26:37 +0100

    Move intel hardware config stuff to host config

--- hosts/sv-minis/configuration.nix
+++ hosts/sv-minis/configuration.nix
@@ -10,9 +10,26 @@
     ../../modules/machine/miniserver
   ];
 
-  # --- Custom Module Settings ---
+  # --- Hardware ---
 
-  dobikoConf.ffmpeg.enabled = false;
+  hardware.enableAllFirmware = true;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-ocl
+      intel-vaapi-driver
+      libva-vdpau-driver
+      intel-compute-runtime-legacy1
+
+      intel-media-driver # This is a second driver for dire times
+    ];
+  };
+  environment.systemPackages = with pkgs; [ intel-gpu-tools ];
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "i965"; };
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver =
+      pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
 
   # --- Bootloader ---
 
