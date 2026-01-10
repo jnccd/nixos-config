0f7aa60814f630bea32f63c0c947661dfeaa1a3f
jnccd <kobidogao@outlook.com>
2026-01-10 14:43:19 +0100

    Make dotfiles copy quiet

--- copy-dotfiles/from-repo-to-home.sh
+++ copy-dotfiles/from-repo-to-home.sh
@@ -9,15 +9,13 @@ else
     # Propagate terminal settings to all users
     while IFS=: read -r user _ uid gid _ home shell; do
     if [ "$home" != "/var/empty" ] && [ "$home" != "/run/dbus" ] && [ "$home" != "/var/lib/davis" ] && [ -d "$home" ]; then
-        echo "$home"
-        
         if [ "$uid" -ge 1000 ]; then
             # Real users get everything
-            sudo rsync -av --chown=$uid:$gid "../dotfiles/" "$home/"
+            sudo rsync -qav --chown=$uid:$gid "../dotfiles/" "$home/"
         else
             # System users get terminal settings
-            sudo rsync -av --chown=$uid:$gid "../dotfiles/.config/nushell/" "$home/.config/nushell/"
-            sudo rsync -av --chown=$uid:$gid "../dotfiles/.config/nvim/" "$home/.config/nvim/"
+            sudo rsync -qav --chown=$uid:$gid "../dotfiles/.config/nushell/" "$home/.config/nushell/"
+            sudo rsync -qav --chown=$uid:$gid "../dotfiles/.config/nvim/" "$home/.config/nvim/"
         fi
     fi
     done < /etc/passwd
