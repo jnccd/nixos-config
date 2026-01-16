3f5781c5fbd81fc879b2f8df8e7b7ec7352a12a3
jnccd <kobidogao@outlook.com>
2026-01-16 17:03:56 +0100

    Separate fail2ban jails

--- modules/generic/common/rev-proxy/fail2ban.nix
+++ modules/generic/common/rev-proxy/fail2ban.nix
@@ -1,13 +1,12 @@
 { config, lib, pkgs, globalArgs, ... }:
 let
-  fail2BanJailName = "nginx-access";
-  fail2BanActionName = "log-bans";
+  notFoundJailName = "nginx-not-found";
+  botJailName = "nginx-bot";
+  logActionName = "log-bans";
 in {
   services.fail2ban = {
     enable = true;
-    maxretry = 4;
     ignoreIP = [ "192.168.0.0/16" ];
-    bantime = "3m";
     bantime-increment = {
       enable = true;
       multipliers = "1 4 8 16 32 64 128 256 512";
@@ -15,24 +14,42 @@ in {
       overalljails = true;
     };
     jails = {
-      "${fail2BanJailName}".settings = {
+      "${notFoundJailName}".settings = {
+        filter = "${notFoundJailName}";
+        maxretry = 7;
+        bantime = "1m";
         enabled = true;
-        filter = "${fail2BanJailName}";
         logpath = "/var/log/nginx/access.log";
         action = ''
           %(action_)s[blocktype=DROP]
-                           ${fail2BanActionName}'';
+                           ${logActionName}'';
+        backend = "auto";
+      };
+      "${botJailName}".settings = {
+        filter = "${botJailName}";
+        maxretry = 2;
+        bantime = "4m";
+        enabled = true;
+        logpath = "/var/log/nginx/access.log";
+        action = ''
+          %(action_)s[blocktype=DROP]
+                           ${logActionName}'';
         backend = "auto";
       };
     };
   };
   environment.etc = {
-    "fail2ban/filter.d/${fail2BanJailName}.local".text = pkgs.lib.mkAfter ''
+    "fail2ban/filter.d/${notFoundJailName}.local".text = pkgs.lib.mkAfter ''
       [Definition]
-      failregex = ^<HOST>.*"(GET|POST|HEAD).*(phpunit|wp-includes|wp-content|wp-trackback|wp-admin|\.\./\.\./| 404 ).*"
+      failregex = ^<HOST>.*"(GET|POST|HEAD).* 404 .*"
       ignoreregex =
     '';
-    "fail2ban/action.d/${fail2BanActionName}.local".text = pkgs.lib.mkAfter ''
+    "fail2ban/filter.d/${botJailName}.local".text = pkgs.lib.mkAfter ''
+      [Definition]
+      failregex = ^<HOST>.*"(GET|POST|HEAD).*(phpunit|wp-includes|wp-content|wp-trackback|wp-admin|\.\./\.\./).*"
+      ignoreregex =
+    '';
+    "fail2ban/action.d/${logActionName}.local".text = pkgs.lib.mkAfter ''
       [Definition]
       norestored = true # Needed to avoid receiving a new notification after every restart
       actionban = echo "$(date) <name> jail has banned <ip> from accessing $(hostname) after <failures> attempts" >> /var/log/fail2ban/ban.log
