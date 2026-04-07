b1c09b066a8c60af80e69debf1bafe1347c394e6
jnccd <kobidogao@outlook.com>
2026-04-07 21:52:49 +0200

    Structure flake inputs a bit

--- flake.nix
+++ flake.nix
@@ -4,25 +4,29 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
+    # --- Core Inputs ---
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # --- Nice to haves ---
+
     kwin-effects-forceblur = {
       url = "github:taj-ny/kwin-effects-forceblur";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # --- My own custom stuff ---
 
     crossgrub-theme = {
       url = "github:jnccd/crossgrub";
