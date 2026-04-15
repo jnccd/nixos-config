9930a6d12d7bd666347fdf2e5e11eb00a0a3cce5
jnccd <kobidogao@outlook.com>
2026-04-15 22:42:18 +0200

    Add sketchy pipewire fix

--- modules/generic/gui/audio.nix
+++ modules/generic/gui/audio.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, globalArgs, ... }: {
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
   services.pipewire = {
@@ -7,5 +7,17 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
+  };
+
+  systemd.services = lib.custom.mkWrappedScreenService {
+    sessionName = "pipewire-pulse-starter";
+    username = globalArgs.mainUser.name;
+    scriptDirName = "pipewire-pulse-starter";
+    wantedBy = [ "graphical.target" ];
+    requires = [ "graphical.target" ];
+    after = [ "graphical.target" ];
+    script = pkgs.writeScript "script" ''
+      WAYLAND_DISPLAY=wayland-0 XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus XDG_DATA_DIRS="/run/current-system/sw/share" pipewire-pulse
+    '';
   };
 }
