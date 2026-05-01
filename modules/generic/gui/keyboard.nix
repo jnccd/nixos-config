64e23f51f9e66e3a4e65d626b7e58923cbe86404
jnccd <kobidogao@outlook.com>
2026-05-01 03:58:43 +0200

    Fix fcitx env vars

--- modules/generic/gui/keyboard.nix
+++ modules/generic/gui/keyboard.nix
@@ -1,14 +1,21 @@
-{ inputs, config, lib, pkgs, globalArgs, ... }: {
+{ inputs, config, lib, pkgs, globalArgs, ... }:
+let
+  defaultLayout = "us-altgr-intl";
+  defaultIM = "keyboard-us-altgr-intl";
+
+  secondaryLayout = "de";
+  secondaryIM = "keyboard-de";
+in {
   options.dobikoConf.fcitx5.layout = lib.mkOption {
     type = lib.types.str;
-    default = "us";
+    default = defaultLayout;
     description = ''
       Keyboard layout used in fcitx5
-      Example: de, us, us-intl, us(altgr-intl)'';
+      Example: de, us, us-intl, us-altgr-intl, us(altgr-intl)'';
   };
   options.dobikoConf.fcitx5.im = lib.mkOption {
     type = lib.types.str;
-    default = "keyboard-us-altgr-intl";
+    default = defaultIM;
     description = ''
       Keyboard input method used in fcitx5
       Example: keyboard-de, keyboard-us, keyboard-us-intl, keyboard-us-altgr-intl
@@ -16,12 +23,17 @@
   };
 
   config = {
-    environment.sessionVariables = { SKIP_FCITX_USER_PATH = ""; };
+    environment.sessionVariables = {
+      #SKIP_FCITX_USER_PATH = "";
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      QT_IM_MODULES = "fcitx";
+    };
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5 = {
-        ignoreUserConfig = false;
+        ignoreUserConfig = true;
         waylandFrontend = true;
         addons = with pkgs; [
           kdePackages.fcitx5-qt
@@ -39,13 +51,14 @@
             "Layout" = config.dobikoConf.fcitx5.layout;
           };
           "Groups/0/Items/1" = {
-            "Name" =
-              if config.dobikoConf.fcitx5.im == "keyboard-us-altgr-intl" then
-                "keyboard-de"
-              else
-                "keyboard-us-altgr-intl";
-            "Layout" =
-              if config.dobikoConf.fcitx5.layout == "us" then "de" else "us";
+            "Name" = if config.dobikoConf.fcitx5.im == defaultIM then
+              secondaryIM
+            else
+              defaultIM;
+            "Layout" = if config.dobikoConf.fcitx5.layout == defaultLayout then
+              secondaryLayout
+            else
+              defaultLayout;
           };
           "Groups/0/Items/2".Name = "pinyin";
           "Groups/0/Items/3".Name = "mozc";
