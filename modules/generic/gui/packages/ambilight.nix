c55caa3c21d10b2f85d1e7dc3d9c9ef727699be1
jnccd <kobidogao@outlook.com>
2026-05-01 04:21:13 +0200

    Add hyperhdr autostart

--- modules/generic/gui/packages/ambilight.nix
+++ modules/generic/gui/packages/ambilight.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, globalArgs, ... }: {
   options.dobikoConf.ambilight.enabled = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -11,5 +11,19 @@
         hyperhdr
 
       ];
+
+    systemd.services = lib.custom.mkWrappedScreenService rec {
+      sessionName = "hyperhdr-starter";
+      username = globalArgs.mainUser.name;
+      scriptDirName = sessionName;
+      wantedBy = [ "graphical.target" ];
+      requires = [ "graphical.target" ];
+      after = [ "graphical.target" ];
+      script = pkgs.writeScript "script" ''
+        sleep 6
+        export ${lib.custom.bashGetGuiVarsForUser globalArgs.mainUser.name} 
+        hyperhdr
+      '';
+    };
   };
 }
