be7af2ad239c8731b799dca08ccc035c1747abbc
jnccd <kobidogao@outlook.com>
2026-05-10 01:58:03 +0200

    Use SystemdTimers for bedtime jobs

--- modules/generic/gui/bedtime.nix
+++ modules/generic/gui/bedtime.nix
@@ -5,42 +5,67 @@
     description = "Forces you to go to bed";
   };
 
-  config = lib.mkIf config.dobikoConf.bedtime.enabled {
+  config = let
+    cronJobToSystemdTimer = { nameStr, timeStr, user, command }: {
+      timer."${nameStr}-${timeStr}-timer" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "${timeStr}";
+          Unit = "${nameStr}-${timeStr}.service";
+        };
+      };
+      service."${nameStr}-${timeStr}" = {
+        description = "Bedtime Shutdown at ${timeStr}";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${command}";
+          User = user;
+        };
+      };
+    };
+
+    bedTimeJobForTime = { bedTimeMin, bedTimeHour, delayMin ? 3 }:
+      let
+        bedTimeMinStr = builtins.toString bedTimeMin;
+        bedTimeHourStr = builtins.toString bedTimeHour;
+      in [
+        (cronJobToSystemdTimer {
+          nameStr = "bedtime-shutdown";
+          timeStr = "${bedTimeHourStr}:${bedTimeMinStr}";
+          user = "root";
+          command = "shutdown +${builtins.toString delayMin}";
+        })
+      ] ++ (map (delayPassed:
+        let warnTimeMinStr = builtins.toString (bedTimeMin + delayPassed);
+        in (cronJobToSystemdTimer {
+          nameStr = "bedtime-shutdown-warn";
+          timeStr = "${bedTimeHourStr}:${warnTimeMinStr}";
+          user = "${globalArgs.mainUser.name}";
+          command = ''
+            ${lib.custom.bashGetUserVars} && kdialog --sorry "Bedtime in ${
+              builtins.toString (delayMin - delayPassed)
+            } minutes." "You really need to go to bed :(" '';
+        })) (builtins.genList (i: i) delayMin));
+
+    bedTimeJobs = (bedTimeJobForTime {
+      bedTimeHour = 22;
+      bedTimeMin = 30;
+    }) ++ (bedTimeJobForTime {
+      bedTimeHour = 23;
+      bedTimeMin = 0;
+    }) ++ (bedTimeJobForTime {
+      bedTimeHour = 23;
+      bedTimeMin = 30;
+    }) ++ (bedTimeJobForTime {
+      bedTimeHour = 0;
+      bedTimeMin = 0;
+    });
+  in lib.mkIf config.dobikoConf.bedtime.enabled {
     environment.systemPackages = with pkgs; [ kdePackages.kdialog ];
 
-    services.cron = let
-      bedTimeToJobs = { bedTimeMin, bedTimeHour, delayMin ? 3 }:
-        [
-          "${builtins.toString bedTimeMin} ${
-            builtins.toString bedTimeHour
-          } * * *      root    shutdown +${
-            builtins.toString delayMin
-          } >> /tmp/cron.log"
-        ] ++ (map (delayPassed:
-          ''
-            ${builtins.toString (bedTimeMin + delayPassed)} ${
-              builtins.toString bedTimeHour
-            } * * *      ${globalArgs.mainUser.name}    ${
-              lib.custom.bashGetGuiVarsForUser globalArgs.mainUser.name
-            } kdialog --sorry "Bedtime in ${
-              builtins.toString (delayMin - delayPassed)
-            } minutes." "You really need to go to bed :(" >> /tmp/cron.log 2>&1'')
-          (builtins.genList (i: i) delayMin));
-    in {
-      enable = true;
-      systemCronJobs = (bedTimeToJobs {
-        bedTimeHour = 22;
-        bedTimeMin = 30;
-      }) ++ (bedTimeToJobs {
-        bedTimeHour = 23;
-        bedTimeMin = 0;
-      }) ++ (bedTimeToJobs {
-        bedTimeHour = 23;
-        bedTimeMin = 30;
-      }) ++ (bedTimeToJobs {
-        bedTimeHour = 0;
-        bedTimeMin = 0;
-      });
-    };
+    systemd.timers =
+      lib.foldl (acc: x: acc // x) { } (builtins.map (x: x.timer) bedTimeJobs);
+    systemd.services = lib.foldl (acc: x: acc // x) { }
+      (builtins.map (x: x.service) bedTimeJobs);
   };
 }
