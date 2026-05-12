225a5898c02397e1b84e727b5c5433d8a310e640
jnccd <kobidogao@outlook.com>
2026-05-12 11:29:54 +0200

    Migrate autostart scripts back to services

--- modules/generic/gui/packages/ambilight.nix
+++ modules/generic/gui/packages/ambilight.nix
@@ -11,5 +11,19 @@
         hyperhdr
 
       ];
+
+    systemd.services = lib.custom.mkWrappedScreenService rec {
+      sessionName = "hyperhdr-starter";
+      username = globalArgs.mainUser.name;
+      scriptDirName = sessionName;
+      wantedBy = [ "graphical.target" ];
+      requires = [ "graphical.target" ];
+      after = [ "graphical.target" ];
+      script = pkgs.writeScript "script" ''
+        sleep 6
+        ${lib.custom.bashGetUserEnvVars globalArgs.mainUser.name} 
+        hyperhdr
+      '';
+    };
   };
 }
--- modules/generic/gui/packages/eyedropper.nix
+++ modules/generic/gui/packages/eyedropper.nix
@@ -11,5 +11,19 @@
         inputs.instant-eyedropper-r.packages."${system}".default
 
       ];
+
+    systemd.services = lib.custom.mkWrappedScreenService rec {
+      sessionName = "eyedropper-starter";
+      username = globalArgs.mainUser.name;
+      scriptDirName = sessionName;
+      wantedBy = [ "graphical.target" ];
+      requires = [ "graphical.target" ];
+      after = [ "graphical.target" ];
+      script = pkgs.writeScript "script" ''
+        sleep 6
+        ${lib.custom.bashGetUserEnvVars globalArgs.mainUser.name} 
+        ie-r
+      '';
+    };
   };
 }
