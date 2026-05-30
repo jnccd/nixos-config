c927f484fe6e2448567ab59638dbf5592ce18740
jnccd <kobidogao@outlook.com>
2026-05-30 14:48:26 +0200

    Add swapfile in lt

--- hosts/lt-coffeelake/hardware-configuration.nix
+++ hosts/lt-coffeelake/hardware-configuration.nix
@@ -23,7 +23,11 @@
     options = [ "fmask=0077" "dmask=0077" ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/swapfile";
+    priority = 1000;
+    size = 8192;
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
