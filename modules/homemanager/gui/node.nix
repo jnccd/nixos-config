7ba559988d63d59467ad1b9e72cb1543f126a22e
jnccd <kobidogao@outlook.com>
2026-08-24 13:55:09 +0200

    Add bashrc in node setup

--- modules/homemanager/gui/node.nix
+++ modules/homemanager/gui/node.nix
@@ -8,6 +8,16 @@ else
       nodejs_22
     ];
 
+    # Set up bashrc
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+        mkdir -p ~/.npm-global
+        npm config set prefix '~/.npm-global'
+        export PATH=~/.npm-global/bin:$PATH
+      '';
+    };
+
     # Those dont seem to work 🤔
     home.sessionPath = [
       "$HOME/.local/share/pnpm/bin"
