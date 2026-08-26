858971dd82f06695a1db2b922bfb428147d66fb5
jnccd <kobidogao@outlook.com>
2026-08-26 19:06:16 +0200

    Lower npm-global/bin prio

--- modules/homemanager/gui/node.nix
+++ modules/homemanager/gui/node.nix
@@ -14,7 +14,7 @@ else
       bashrcExtra = ''
         mkdir -p ~/.npm-global
         npm config set prefix '~/.npm-global'
-        export PATH=~/.npm-global/bin:$PATH
+        export PATH=$PATH:~/.npm-global/bin
       '';
     };
 
